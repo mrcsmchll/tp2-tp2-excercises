47a3f23855aaba2a9846d6173ee811ad5ba952b7
mrcsmchll <manchinelli.m@gmail.com>
2022-10-30 20:21:02 -0300

    added try/catch to manage error

--- sync-async-fileSystem/scripts/mac.js
+++ sync-async-fileSystem/scripts/mac.js
@@ -3,26 +3,30 @@
 import fs, { stat } from 'fs'
 
 const mac = filePath => {
-    fs.readFile(filePath, 'utf-8', (err, data) => {
-        if (err) throw new Error(`error en lectura de archivo ${err.message}`)
-        let info = {
-            contenidoStr: JSON.stringify(data, null, "\t"),
-            contenidoObj: JSON.parse(data),
-            size: null
+    try {
+        fs.readFile(filePath, 'utf-8', (err, data) => {
+            if (err) throw new Error(`error en lectura de archivo ${err.message}`)
+            let info = {
+                contenidoStr: JSON.stringify(data, null, "\t"),
+                contenidoObj: JSON.parse(data),
+                size: null
+            }
+            fs.stat(filePath, (err, stat) => {
+                if (err) throw new Error(`error en la obtencion del size: ${err.message}`)
+                info.size = stat.size
+                console.log(info)
+            })
+
+            fs.writeFile("infoMac.txt", JSON.stringify(info, null, "\t"), "utf-8", (err) => {
+                if (err) throw new Error(`error al crear archivo txt: ${err.message}`)
+            })
+
         }
-        fs.stat(filePath, (err, stat) => {
-            if (err) throw new Error(`error en la obtencion del size: ${err.message}`)
-            info.size = stat.size
-            console.log(info)
-        })
-
-        fs.writeFile("infoMac.txt", JSON.stringify(info, null, "\t"), "utf-8", (err) => {
-            if (err) throw new Error(`error al crear archivo txt: ${err.message}`)
-        })
-
+        )
+    } catch (err) {
+        console.log(`Error en mac: ${err.message}`)
+        }
     }
-    )
-}
 
 export default mac
 
