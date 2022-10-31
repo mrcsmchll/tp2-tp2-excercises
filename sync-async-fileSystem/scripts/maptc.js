6aa2cf9b545e5d21e2a11dd9c3c5eb04054b302c
mrcsmchll <manchinelli.m@gmail.com>
2022-10-30 22:48:22 -0300

    finished maptc.js & see comments in tests/test.js

--- sync-async-fileSystem/scripts/maptc.js
+++ sync-async-fileSystem/scripts/maptc.js
@@ -1,1 +1,34 @@
 /* 3 --- File System en Modo Asincronico c/ Promises + Then-Catch --- */
+
+import fs from 'fs'
+
+const maptc = filepath => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(filepath, "utf-8", (err, data) => {
+            if (err) reject(err)
+            fs.stat(filepath, (err, stat) => {
+                if (err) reject(err)
+                let info = {
+                    contenidoStr: JSON.stringify(data, null, "\t"),
+                    contenidoObj: JSON.parse(data),
+                    size: stat.size
+                }
+                fs.writeFile("infoMaptc.txt", JSON.stringify(info, null, "\t"), err => {
+                    reject(err)
+                })
+                resolve(info)
+            })
+        })
+    })
+}
+
+/* Then/Catch should be use in test.js */
+maptc("../package.json").then(data => {
+    console.log(data)
+}).catch(err => {
+    console.log(`error en maptc: ${err.message}`)
+})
+
+export default maptc
+
+
--- sync-async-fileSystem/tests/test.js
+++ sync-async-fileSystem/tests/test.js
@@ -1,9 +1,12 @@
 import ms from "../scripts/ms.js"
 import mac from "../scripts/mac.js"
+import maptc from "../scripts/maptc.js"
 
 const path = "../package.json"
 
-
-
+/* transfer try-catch from ms */
 // ms(path)
-mac(path)
+/* transfer try-catch from mac */
+// mac(path)
+/* transfer then-catch from maptc */
+// maptc(path)
