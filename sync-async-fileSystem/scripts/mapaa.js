1a9d473401795c880d42c2a19139aba3afbfce72
mrcsmchll <manchinelli.m@gmail.com>
2022-10-31 17:53:53 -0300

    finished mapaa.js with fs.promises interface

--- sync-async-fileSystem/scripts/mapaa.js
+++ sync-async-fileSystem/scripts/mapaa.js
@@ -1,10 +1,16 @@
 /* 4 --- File System en Modo Asincronico c/ Callbacks + Async-Await --- */
 import fs from 'fs'
 
-const mapaa = filePath => {
-    
-    
-
+const mapaa = async filePath => {
+    let info = {
+        contenidoStr: JSON.stringify(await fs.promises.readFile(filePath, "utf-8"), null, "\t"),
+        contenidoObj: JSON.parse(await fs.promises.readFile(filePath)),
+        size: (await fs.promises.stat(filePath)).size
+    }
+    console.log(info)
+    await fs.promises.writeFile("infoMapaa.txt", JSON.stringify(info, null, "\t"))
 }
+
+// mapaa("../package.json")
 
 export default mapaa
